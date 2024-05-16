3bf57d56f9d58b9e062c6740414f10cbddaf038d
greg <>
2024-05-16 18:43:35 +0200

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+const logs = document.getElementById("logs")
+
+function notify() {
+  setTimeout(() => {
+    const n = new Notification("Hello")
+    n.onshow = () => logs.insertAdjacentHTML("afterend", `<p>Shown ${new Date().toISOString()}</p>`)
+    n.onclick = () => logs.insertAdjacentHTML("afterend", `<p>Clicked ${new Date().toISOString()}</p>`)
+    n.onclose = () => logs.insertAdjacentHTML("afterend", `<p>Closed ${new Date().toISOString()}</p>`)
+    n.onerror = () => logs.insertAdjacentHTML("afterend", `<p>Error ${new Date().toISOString()}</p>`)
+  }, 1000)
+}
