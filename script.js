242851f0c9c5a5b15297457bc8be4acd2cc06a4e
greg <>
2024-05-16 19:06:19 +0200

    show notification permission

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function notify() {
     n.onshow = () => logs.insertAdjacentHTML("afterend", `<p>Shown ${new Date().toISOString()}</p>`)
     n.onclick = () => logs.insertAdjacentHTML("afterend", `<p>Clicked ${new Date().toISOString()}</p>`)
     n.onclose = () => logs.insertAdjacentHTML("afterend", `<p>Closed ${new Date().toISOString()}</p>`)
-    n.onerror = () => logs.insertAdjacentHTML("afterend", `<p>Error ${new Date().toISOString()}</p>`)
+    n.onerror = () =>
+      logs.insertAdjacentHTML("afterend", `<p>Error ${new Date().toISOString()} ${Notification.permission}</p>`)
   }, 1000)
 }
