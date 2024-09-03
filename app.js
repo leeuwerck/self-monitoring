a24a070f53abee37abbf347052840174b88a1d56
greg <>
2024-09-03 16:25:25 +0200

    try creating store from main js

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const logs = document.getElementById("logs")
+navigator.serviceWorker.register("sw.js")
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js")
+const SERVICE_WORKER_MESSAGES = {
+  START_NOTIFYING: "start_notifying",
+  STOP_NOTIFYING: "stop_notifying",
 }
+
+function startBackgroundNotifications() {
+  sendServiceWorkerMessage(SERVICE_WORKER_MESSAGES.START_NOTIFYING)
+  document.getElementById("running_state_display").innerHTML = "Running"
+}
+
+function stopBackgroundNotifications() {
+  sendServiceWorkerMessage(SERVICE_WORKER_MESSAGES.STOP_NOTIFYING)
+  document.getElementById("running_state_display").innerHTML = "Paused"
+}
+
+function sendServiceWorkerMessage(message) {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.active.postMessage(message)
+  })
+}
+
+const logs = document.getElementById("logs")
 
 function notify() {
   const delaySeconds = parseInt(document.getElementById("delay_seconds").value)
@@ -14,4 +33,34 @@ function notify() {
     n.onerror = () =>
       logs.insertAdjacentHTML("afterend", `<p>Error ${new Date().toISOString()} ${Notification.permission}</p>`)
   }, delaySeconds * 1000)
+}
+
+let db
+const request = indexedDB.open("experiment", 1)
+
+request.addEventListener("upgradeneeded", (init) => {
+  db = init.target.result
+
+  db.onerror = () => {
+    console.error("Error loading database.")
+  }
+
+  const table = db.createObjectStore("notifications")
+
+  table.createIndex("content", "content", { unique: false })
+  table.createIndex("reaction", "reaction", { unique: false })
+})
+
+request.onsuccess = function () {
+  db = request.result
+}
+
+function getNotifications() {
+  const transaction = db.transaction("notifications", "readonly")
+  const objectStore = transaction.objectStore("notifications")
+  objectStore.getAll().onsuccess = function (event) {
+    document.getElementById("notifications").innerHTML = event.target.result
+      .map((line) => `${JSON.stringify(line)}`)
+      .join("\n")
+  }
 }
--- sw.js
+++ sw.js
@@ -1,15 +1,82 @@
+const NOTIFICATION_DISPLAY_DURATION = 3000
+const NOTIFICATION_FREQUENCY = 2000
+
+let db
+const openOrCreateDB = indexedDB.open("experiment", 1)
+
+openOrCreateDB.addEventListener("error", () => console.error("Error opening DB"))
+
+openOrCreateDB.addEventListener("success", () => {
+  console.log("Successfully opened DB")
+  db = openOrCreateDB.result
+})
+
+openOrCreateDB.addEventListener("upgradeneeded", (init) => {
+  db = init.target.result
+
+  db.onerror = () => {
+    console.error("Error loading database.")
+  }
+
+  const table = db.createObjectStore("notifications")
+
+  table.createIndex("content", "content", { unique: false })
+  table.createIndex("reaction", "reaction", { unique: false })
+})
+
+let running = false
+
 self.addEventListener("notificationclick", (event) => {
   console.log("clicked", event)
+  addNotification(event?.notification?.tag, "none", "clicked")
+
   event.notification.close()
 })
 self.addEventListener("notificationclose", (event) => {
+  addNotification(event?.notification?.tag, "none", "closed")
   console.log("closed", event)
 })
 
+self.addEventListener("message", (event) => {
+  switch (event.data) {
+    case "start_notifying":
+      running = true
+      notify()
+      break
+    case "stop_notifying":
+      running = false
+      break
+
+    default:
+      console.error(event.data, "no matching event type")
+  }
+})
+
 function notify() {
-  console.log("notify")
-  self.registration.showNotification("hello sw")
-  setTimeout(notify, 5000)
+  if (!running) return
+  const randomId = self.crypto.randomUUID()
+  console.log("notify ", randomId)
+  self.registration.showNotification("SW", { body: `Body ${randomId}`, tag: randomId }).then(() =>
+    new Promise((resolve) => setTimeout(resolve, NOTIFICATION_DISPLAY_DURATION)) // keep service worker alive
+      .then(() => self.registration.getNotifications())
+      .then((notifications) => {
+        const notification = notifications.find((notification) => notification.tag === randomId)
+        if (notification) {
+          addNotification(notification.tag, "none", "expired")
+          notification.close()
+        }
+      })
+  )
+  setTimeout(notify, NOTIFICATION_FREQUENCY)
 }
 
-notify()
+function addNotification(id, content, reaction) {
+  const newNotification = { id, content, reaction, displayDate: new Date() }
+  const transaction = db.transaction(["notifications"], "readwrite")
+  const objectStore = transaction.objectStore("notifications")
+  const query = objectStore.add(newNotification)
+  transaction.addEventListener("complete", () => {
+    console.log("notification saved", newNotification)
+  })
+  transaction.addEventListener("error", () => console.log("Transaction error"))
+}
